03c3baf54c1cca77b96db381e225d1f1bbf3f61d
郝天明 <nimbly-spilt-bok@duck.com>
2024-10-19 12:15:25 +0100

    Update SuperIPTVPlugin.kt

--- SuperIPTV/src/main/kotlin/com/HaoTianming/SuperIPTVPlugin.kt
+++ SuperIPTV/src/main/kotlin/com/HaoTianming/SuperIPTVPlugin.kt
@@ -8,7 +8,7 @@ class QuickIPTVPlugin: Plugin() {
     override fun load(context: Context) {
         // All providers should be added in this manner. Please don't edit the providers list directly.
         registerMainAPI(PublicIPTV())
-        registerMainAPI(PublicIPTVJapan1())
+        registerMainAPI(PublicJapanIPTV1())
         registerMainAPI(PublicAsianIPTV1())
         registerMainAPI(PublicAsianIPTV2())
         registerMainAPI(PublicSportsIPTV1())
