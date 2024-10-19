71a06fc8d9ed3ee119b645864ce08cb2d1032e27
郝天明 <nimbly-spilt-bok@duck.com>
2024-10-19 11:53:36 +0100

    Update SuperIPTVPlugin.kt

--- SuperIPTV/src/main/kotlin/com/HaoTianming/SuperIPTVPlugin.kt
+++ SuperIPTV/src/main/kotlin/com/HaoTianming/SuperIPTVPlugin.kt
@@ -1,1 +1,15 @@
+package com.HaoTianming
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
 
+@CloudstreamPlugin
+class QuickIPTVPlugin: Plugin() {
+    override fun load(context: Context) {
+        // All providers should be added in this manner. Please don't edit the providers list directly.
+        registerMainAPI(PublicIPTV())
+        registerMainAPI(())
+        registerMainAPI(())
+        registerMainAPI(())
+    }
+}
